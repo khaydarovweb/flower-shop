929c49689d69fe545194c71d05db54c95ff09ecb
abduvali <abduvalikhaydarov005@gmail.com>
2024-02-24 22:49:14 +0500

    login with ui

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,6 +1,8 @@
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../modules/auth/context';
 
+import { FaHouse } from 'react-icons/fa6';
+
 import LoginLeft from '../../assets/imgs/login.png';
 
 interface LoginProps {}
@@ -24,26 +26,41 @@ const Login = (props: LoginProps) => {
 
 	return (
 		<section className="h-screen flex">
-			<div className="p-10 w-2/4">
+			<div className="p-14 w-2/4">
 				<form className="m-2" onSubmit={handleLogin}>
 					<span className="w-full flex justify-between">
-						<h3 className="font-medium text-[32px]">Login</h3>
-						<button onClick={() => navigate('/')}>Back to home</button>
+						<span>
+							<h3 className="font-medium text-[52px]">Login</h3>
+							<p className="font-[32px] text-[#838383]">Login and have more fun</p>
+						</span>
+						<button className="flex items-center gap-2" onClick={() => navigate('/')}>
+							{' '}
+							<FaHouse color="#FF8F52" />
+							Back to home
+						</button>
 					</span>
-					<input
-						className="bg-white text-black border border-gray-500 rounded-md p-2 outline-none"
-						placeholder="Email..."
-						name="email"
-					/>
-					<input
-						className="bg-white text-black border border-gray-500 rounded-md p-2 outline-none"
-						placeholder="Password..."
-						name="password"
-					/>
+					<span className="flex flex-col py-5 gap-5">
+						<input
+							className="bg-white border border-[#D9D9D9] rounded-md p-2 outline-none font-[22px] text-[#838383]"
+							placeholder="Email..."
+							name="email"
+						/>
+						<input
+							className="bg-white border border-[#D9D9D9] rounded-md p-2 outline-none font-[22px] text-[#838383]"
+							placeholder="Password..."
+							name="password"
+						/>
 
-					<button className="bg-yellow-500 text-white rounded-md px-4 py-2 mt-2" type="submit">
-						Login
-					</button>
+						<button className="bg-orange-400 text-white rounded-md px-4 py-2 mt-2" type="submit">
+							Login
+						</button>
+					</span>
+					<p className="text-center">
+						Do not have an account?{' '}
+						<span className="text-[#FF8F52]" onClick={() => navigate('/auth/register')}>
+							Register
+						</span>
+					</p>
 				</form>
 			</div>
 			<div className="w-2/4">
